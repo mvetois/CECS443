37c84f74dc2eefabe1ea78113b7a247b1993ed85
Matthieu VETOIS <matthieu.vetois@epitech.eu>
2022-03-12 22:41:30 -0800

    Upd : Update tags for ping route

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -14,6 +14,7 @@ const router : Router = Router();
  *   get:
  *     summary: Ping the API.
  *     description: Check if the API is online.
+ *     tags: [Ping]
  *     responses:
  *       200:
  *         description: The API response.
