58901ca9a57ad2af57d5b99906738ee591ddbfa9
Matthieu VETOIS <matthieu.vetois@epitech.eu>
2022-03-12 22:09:58 -0800

    Add : Add the User model for the DB

---
+++ back/src/models/User.model.ts
@@ -0,0 +1,20 @@
+/* ----- Imports ----- */
+
+import mongoose, { Schema, Model } from "mongoose";
+
+/* ----- Code ----- */
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+});
+
+export const User = mongoose.model("User", userSchema);
