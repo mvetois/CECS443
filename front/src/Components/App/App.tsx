06e3b06ef5254a16b45ccf7e1f77a3bd5707db80
Nathan Taylor <nathan.taylor562@gmail.com>
2022-03-17 12:25:54 -0700

    Fixed compilation errors

--- front/src/Components/App/App.tsx
+++ front/src/Components/App/App.tsx
@@ -10,14 +10,16 @@ import React, {Component} from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
-class App = () => {
-
+class App extends Component {
+    render() {
         return (
             <div className="App">
                 <header className="App-header">
+                    Hello World!
+                </header>
             </div>
         )
-
+    }
 }
 
 export default App;
