d49d5e606092eabfabffee0a2bf427b3b0aab9a8
Matthieu VETOIS <matthieu.vetois@epitech.eu>
2022-03-21 20:49:06 -0700

    Add : Adding token verification for admins

--- back/src/helpers/jtw.ts
+++ back/src/helpers/jtw.ts
@@ -9,9 +9,12 @@ const secret : Secret= "secret";
 
 export const signAccessToken = (userID : string, admin : boolean) : Promise<any> => {
     return (new Promise((resolve, reject) => {
-        const payload : object = {};
+        const payload : object = {
+            admin: admin,
+            userID: userID
+        };
         const options : SignOptions = {
-            audience: admin + "//" + userID
+            audience: admin + "//" + userID,
         };
         JWT.sign(payload, secret, options, (err: Error, token: string) => {
             if (err) { reject(err) };
@@ -28,6 +31,22 @@ export const verifyAccessToken = (req, res, next) => {
     const token = barer[1];
     JWT.verify(token, secret, (err, payload) => {
         if (err) {
+            return (next(res.status(401).send({error: "Invalid access token"})));
+        }
+        req.payload = payload;
+        next()
+    });
+}
+
+export const verifyAccessTokenAdmin = (req, res, next) => {
+    if (!req.headers["authorization"]) {
+        return(next(res.status(401).send({error: "Access token is required"})));
+    }
+    const barer =  req.headers['authorization'].split(' ');
+    const token = barer[1];
+    console.log(token);
+    JWT.verify(token, secret, (err, payload) => {
+        if (err || !payload.admin) {
             return (next(res.status(401).send({error: "Invalid access token"})));
         }
         req.payload = payload;
