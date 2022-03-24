aa10b4ab2c51819e72a03796998810b6f41800b8
Matthieu VETOIS <matthieu.vetois@epitech.eu>
2022-03-24 16:16:09 -0700

    Add : Adding route to remove a data insite a subcategory

--- back/src/routes/admin/data/index.ts
+++ back/src/routes/admin/data/index.ts
@@ -3,12 +3,14 @@
 import { Router } from "express";
 
 import Add from "./add.data.admin";
+import Rem from "./rem.data.admin";
 
 /* ----- Code ----- */
 
 const router : Router = Router();
 
 router.use("/add", Add);
+router.use("/rem", Rem);
 
 /**
  * @swagger
