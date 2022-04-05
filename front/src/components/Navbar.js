46fa584f9b82aaf039ec3fafb75347b89632bf46
Setha9 <80129057+setha9@users.noreply.github.com>
2022-04-05 15:12:27 -0700

    Adding Logo

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import VectorIcon from '../assets/Vector.png';
+import VectorIcon1 from '../assets/Vector1.png';
 import { useNavigate } from 'react-router-dom';
 import NavBarDropDown from './NavBarDropDown';
 import ThemeToggle from './ThemeToggle';
@@ -22,10 +23,12 @@ const Navbar = () => {
                 height: 80
             }}>
             <img src={VectorIcon} width="150" />
+            <img src={VectorIcon1} width="150" />
             <div style={{ display: 'flex', alignItems: 'center'}}>
                 <NavBarDropDown options={options} />
                 <ThemeToggle colorOne="black" colorTwo="white" />
             </div>
+
         </div>
     );
 }
