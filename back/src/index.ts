071dd04c911f933d5ef5028d08f777e09d26b211
Matthieu VETOIS <matthieu.vetois@epitech.eu>
2022-04-21 16:23:10 -0700

    Upd : Update the post limit to 50mb

--- back/src/index.ts
+++ back/src/index.ts
@@ -18,7 +18,8 @@ app.listen(port, () => {
     console.log("API is running on port " + port);
 });
 
-app.use(express.json());
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({limit: "50mb", extended: true}));
 
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
